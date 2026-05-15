11342f7023b2dd87bc99cc5ab3725cf4c24581c6
tapansonowalofficial-rgb <tapansonowalofficial@gmail.com>
2026-05-15 17:16:26 +0530

    Create app/page.js

---
+++ app/page.js
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#000",
+        color: "#00ffcc",
+        fontFamily: "Arial"
+      }}
+    >
+      <div>
+        <h1>Health ID App Working Successfully 🚀</h1>
+      </div>
+    </main>
+  );
+}
